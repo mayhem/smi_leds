9610bf13a8a8662d5c4edab0e49d54048918fc1b
Robert Kaye <rob@musicbrainz.org>
2024-05-07 17:32:57 +0200

    Add wheel as dep

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, Extension
 
 setup(name = "smi_leds",
       version = "1.0",
-      ext_modules = [Extension("smi_leds", ["module.c", "libsmi_leds.c", "rpi_pixleds_lib.c", "rpi_dma_utils.c"])]
+      ext_modules = [Extension("smi_leds", ["module.c", "libsmi_leds.c", "rpi_pixleds_lib.c", "rpi_dma_utils.c"])],
+      install_requires=[ 'wheel' ]
 )
