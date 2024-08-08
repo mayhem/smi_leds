b6bfd9babea941789dd3659bfaf0e25ab963c3d9
Robert Kaye <rob@musicbrainz.org>
2024-08-08 23:58:51 +0200

    Build the whole extension using setuptools. much simpler!

--- python/setup.py
+++ python/setup.py
@@ -1,60 +1,33 @@
 #!/usr/bin/env python3
 
-import os
-import pathlib
+import sys
 from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext as build_ext_orig
+from subprocess import check_output
 
-class CMakeExtension(Extension):
-    def __init__(self, name):
-        # don't invoke the original build_ext for this special extension
-        super().__init__(name, sources=[])
+def get_compile_flags():
+    try:
+        flags = check_output(["../detect_rpi.py"])
+    except CalledProcessError:
+        print("Cannot determine RPi version. Is this running on an RPi?")
+        sys.exit(-1)
 
-class build_ext(build_ext_orig):
-    def run(self):
-        for ext in self.extensions:
-            self.build_cmake(ext)
-        super().run()
+    print(flags)
 
-    def build_cmake(self, ext):
-        cwd = pathlib.Path().absolute()
-        cmake_dir = os.path.join(cwd, "..")
+    return flags
 
-        # these dirs will be created in build_py, so if you don't have
-        # any python sources to bundle, the dirs will be missing
-        build_temp = pathlib.Path(self.build_temp)
-        build_temp.mkdir(parents=True, exist_ok=True)
-        extdir = pathlib.Path(self.get_ext_fullpath(ext.name))
-        extdir.mkdir(parents=True, exist_ok=True)
-        
-        # example of cmake args
-        config = 'Debug' if self.debug else 'Release'
-        cmake_args = [
-            '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY='+str(extdir.parent.absolute()),
-            '-DCMAKE_BUILD_TYPE=' + config,
-            '-DBUILD_PYTHON_MODULE=ON',
-        ]
-        
-        # example of build args
-        build_args = [
-            '--config', config, 
-        ]
-        
-        os.chdir(str(build_temp))
-        self.spawn(['cmake', str(cmake_dir)] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.'] + build_args)
-        # Troubleshooting: if fail on line above then delete all possible 
-        # temporary CMake files including "CMakeCache.txt" in top level dir.
-        os.chdir(str(cwd))
+
+compile_flags = str(get_compile_flags(), "utf-8").split(" ")
+print(compile_flags)
+compile_flags.append("-DLED_NCHANS=8")
 
 setup(name = "smi_leds",
       version = "1.0",
-      install_requires=[ 'wheel' ],
-      ext_modules = [Extension("smi_leds"),
-                     Extension("smi_leds",
-                               ["module.c", "libsmi_leds.c"],
-                               libraries=["smi_leds"],
-                               library_dirs=["/usr/local/lib"])],
-      cmdclass={'build_ext': build_ext}
+      ext_modules = [Extension("smi_leds",
+                               ["module.c",
+                               "libsmi_leds.c",
+                               "../smi_led/rpi_dma_utils.c",
+                               "../smi_led/rpi_pixleds_lib.c"],
+                               extra_compile_args=compile_flags,
+                               include_dirs=["../include"])],
+      install_requires=[ 'wheel' ]
 )
