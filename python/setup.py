647d71d5a1eb7b8dd0a48446bf93f174a67b7956
Robert Kaye <rob@musicbrainz.org>
2024-08-09 00:57:39 +0200

    Set version numbers

--- python/setup.py
+++ python/setup.py
@@ -34,7 +34,7 @@ def get_compile_flags():
 
 compile_flags = get_compile_flags()
 setup(name = "smi_leds",
-      version = "0.1",
+      version = "2024.08.08.0",
       ext_modules = [Extension("smi_leds",
                                ["module.c",
                                "libsmi_leds.c",
